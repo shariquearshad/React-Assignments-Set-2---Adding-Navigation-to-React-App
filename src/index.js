c3185001f92e2f5557e574d053d41f1bcf6ee2cd
shariquearshad <shariquearshad@gmail.com>
2020-12-16 23:45:24 +0530

    change2

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter, Route } from "react-router-dom";
-function generateCodeFromObject(obj) {
-  return(
-      
-  )
-}
+function generateCodeFromObject(obj) {}
 
 module.exports = generateCodeFromObject;
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // ReactDOM.render(<App />, document.getElementById("root"));
